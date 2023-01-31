c91d15aa4b12d0042ec942126a03a80e1eb6f265
Gerard Baholli <gerardbaholli@live.it>
2023-01-31 22:56:39 +0100

    Add lessons from 20 to 24

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] Animator unitAnimator;
     [SerializeField] float moveSpeed = 4f;
+    [SerializeField] float rotateSpeed = 10f;
 
     private Vector3 targetPosition;
     private float stoppingDistance = .1f;
 
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
+
     private void Update()
     {
+
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+            unitAnimator.SetBool("IsWalking", true);
         }
-
-        if (Input.GetMouseButtonDown(0))
+        else
         {
-            Move(MouseWorld.GetPosition());
+            unitAnimator.SetBool("IsWalking", false);
         }
     }
 
-    private void Move(Vector3 targetPosition)
+    public void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
     }
